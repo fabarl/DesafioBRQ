2ac944b1f482c8eb8ee9994b9f14bbfda81697fa
Fabiano da Silva Oliveira <fabianojuara@hotmail.com>
2022-06-14 12:30:00 -0400

    Corrigindo Validadores de Entrada

--- Peso.cs
+++ Peso.cs
@@ -11,19 +11,23 @@ namespace IMC
         {
                 if (nome == null || nome == " " )
                 {
-                    Console.Write("Nome Inválido! Insira o Nome Novamente: ");
+                    Console.Write("Nome Inválido! \r\nInsira o Nome Novamente: ");
                     VerificaNome(Console.ReadLine()); // recursividade até receber um valor válido!
                 }
          return nome;
         }
         public static string VerificaSexo(string sex) // Função para verificar se foi informado o Sexo Corretamente
         {
-            if((sex != "M" || sex != "m") && (sex != "F" || sex != "f"))
+            string sexo="";
+            if(sex.ToUpper() != "M"  && sex.ToUpper() != "F" )
             {
-                Console.Write("[ERRO] Sexo inválido! Informe a Sexualidade [M] Masculino, [F] Feminino:  ");
+                Console.Write("[ERRO] Sexo inválido! \r\nInforme o sexo [M] Masculino ou [F] Feminino:  ");
                 VerificaSexo(Console.ReadLine()); // recursividade para obter um sexo válido
             }
-            return sex;
+            if (sex.ToUpper() == "M") sexo = "Masculino";
+            if (sex.ToUpper() == "F") sexo = "Feminino";
+
+            return sexo;
         }
         public static int VerificaIdade(int num) // Função para verificar se a idade informada é válida
         {
@@ -31,16 +35,18 @@ namespace IMC
             {
                 if(num <= 0) // verifica se o valor é negativo ou 0
                 {
-                    Console.Write("[ERRO] Idade Inválida, Digite Novamente: ");
+                    Console.Write("[ERRO] Idade Inválida, \r\nDigite Novamente: ");
                     VerificaIdade(int.Parse(Console.ReadLine()));
                 }
+                return num;
             }
             catch //erro caso o dado informado não seja um numero inteiro
             {
-                Console.Write("[ERRO] Idade Inválida, Digite Novamente: ");
+                Console.Write("[ERRO] Idade Inválida, \r\nDigite Novamente: ");
                 VerificaIdade(int.Parse(Console.ReadLine()));
+                return 0;
             }
-            return num;
+            
         }
         public static double VerificaAltura(double alt) // Função para Verificar a Altura informáda
         {
@@ -48,13 +54,13 @@ namespace IMC
             {
                 if(alt <= 0)
                 {
-                    Console.Write("[ERRO] Altura Inválida! Informe a Altura: ");
+                    Console.Write("[ERRO] Altura Inválida! \r\nInforme a Altura: ");
                     VerificaAltura(double.Parse(Console.ReadLine()));
                 }
             }
             catch
             {
-                Console.Write("[ERRO] Altura Inválida! Informe a Altura: ");
+                Console.Write("[ERRO] Altura Inválida! \r\nInforme a Altura: ");
                 VerificaAltura(double.Parse(Console.ReadLine()));
             }
             return alt;
@@ -65,13 +71,13 @@ namespace IMC
             {
                 if(pes <= 0)
                 {
-                    Console.Write("[ERRO] Peso inválido! Informe o Peso: ");
+                    Console.Write("[ERRO] Peso inválido! \r\nInforme o Peso: ");
                     VerificaPeso(double.Parse(Console.ReadLine())); //recursividade até receber o peso correto
                 }
             }
             catch
             {
-                Console.Write("[ERRO] Peso inválido! Informe o Peso: ");
+                Console.Write("[ERRO] Peso inválido! \r\nInforme o Peso: ");
                 VerificaPeso(double.Parse(Console.ReadLine())); //recursividade até receber o peso correto
             }
             return pes;
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 
 namespace IMC
 {
@@ -7,15 +8,22 @@ namespace IMC
     {
         static void Main(string[] args)
         {
-
+            // Entrada de Dados
             Console.Write("Informe o Nome: ");  var nome = Peso.VerificaNome(Console.ReadLine());
-            Console.WriteLine("Para o Calculo de IMC necessitamos saber o Sexo de nascimento, Devido a isso e Somente por isso limitamos a resposta a 2 Opções!");
-            Console.Write("Informe o Sexo, [M] Masculino, [F] Feminino: "); var sexo = Peso.VerificaSexo(Console.ReadLine());
+            Console.WriteLine("#### Para o Calculo de IMC necessitamos saber o Sexo de nascimento ### \r\n### Devido a isso e Somente por isso limitamos a resposta a 2 Opções! ###");
+            Console.Write("Informe o Sexo, [M] Masculino ou [F] Feminino: "); var sexo = Peso.VerificaSexo(Console.ReadLine());
             Console.Write("Informe a Idade: "); var idade = Peso.VerificaIdade(int.Parse(Console.ReadLine()));
             Console.Write("Informe a Altura: ");    var altura = Peso.VerificaAltura(double.Parse(Console.ReadLine()));
             Console.Write("Informe o Peso: ");  var peso = Peso.VerificaPeso(double.Parse(Console.ReadLine()));
             Console.Clear();
-
+            // Fim Entrada de Dados
+            // Exibir os Dados
+            Console.WriteLine("DIAGNÓSTICO PRÉVIO \r\n");
+            Console.WriteLine($"Nome: {nome}");
+            Console.WriteLine($"Sexo: {sexo}");
+            Console.WriteLine($"Idade: {idade}");
+            Console.WriteLine($"Altura: {altura.ToString(CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"Peso: {peso.ToString(CultureInfo.InvariantCulture)}");
 
         }
     }
